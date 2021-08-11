51176a5fbe4fd1d6a95f0ad2f6962b3b65579cd5
Rohit Kumar <shutupRohit@gmail.com>
2021-08-12 01:53:54 +0530

    setting up folder structure

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config();
+}
+
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const flash = require('connect-flash');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+
+const User = require('./models/user');
 
 
 const userRoutes = require('./routes/user');
 
+
+
+
+const ExpressError = require('./utils/ExpressError');
 
 
 
@@ -19,7 +36,11 @@ const userRoutes = require('./routes/user');
 
 
 const PORT = 8080;
-const dbUrl = 'mongodb://localhost:27017/Roadmap-Creator';
+// const dbUrl = 'mongodb://localhost:27017/Roadmap-Creator';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/Roadmap-Creator'
+const clientID = process.env.CLIENTID;
+const clientSecret = process.env.CLIENTSECRET;
+const SECRET = process.env.SECRET || 'thisisasecret';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
@@ -34,25 +55,109 @@ mongoose.connect(dbUrl, {
         console.log('IN MONGOOSE SOMETHING WENT WRONG', err);
     })
 
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    secret: SECRET,
+    touchAfter: 24 * 60 * 60
+})
+
+store.on('error', function (e) {
+    console.log("SESSION STORE ERROR", e);
+})
+
+const sessionConfig = {
+    store,
+    name: 'session',
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie : {
+        expires: Date.now() + 1000*60*60*24*7,
+        maxAge : 1000*60*60*24*7,
+        httpOnly: true,
+    }
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+      done(null, user);
+    });
+});
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: clientID,
+            clientSecret: clientSecret,
+            callbackURL: "/login/google/redirect"
+        }, (accessToken, refreshToken, profile, done) => {
+            // console.log(profile);
+            const displayName = profile.displayName
+            const emailId = profile.emails[0].value;
+            // passport callback function
+            //check if user already exists in our db with the given profile ID
+            User.findOne({ googleId: profile.id }).then(async (currentUser) => {
+                if (currentUser) {
+                    //if we already have a record with the given profile ID
+                    done(null, currentUser);
+                } else {
+                    //if not, create a new user 
+                    const registeredUser = new User({
+                        googleId: profile.id,
+                        name: displayName,
+                        emailId: emailId,
+                    });
+                    await registeredUser.save();
+                    await done(null, registeredUser);
+                }
+            })
+        })
+);
+
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash('error');
+    res.locals.currentUser = req.user;
+    next();
+})
+
+
 
 app.use('/', userRoutes);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.get('/', (req, res) => {
-    res.send("THIS IS OUR TEMPORARY HOME PAGE !!!");
+    res.render('homePage');
+    // res.send("this is our temporary home page")
 })
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError("Page not found !", 404));
+})
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500, message = "Something went wrong"} = err;
+    res.status(statusCode).render('error', {err});
+    res.send();
+})
+
 
 app.listen(PORT, () => {
     console.log(`LISTENING ON PORT ${PORT}`);
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/register', (req, res) => {
+    res.render('user/register');
+})
 
-
+router.get('/login', (req, res) => {
+    res.render('user/login');
+})
 
 
 
